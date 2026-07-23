5ea27db749d775fcc46f73f19c9361c9d9970036
Rotshila Luvhengo <luvhengork@gmail.com>
2026-07-23 02:38:43 +0200

    Refactor loan default risk prediction app

--- app.py
+++ app.py
@@ -171,10 +171,10 @@ elif prob<0.4:
      st.success("Low risk of default. Credit profile looks good. Manual verification is reccomended")
         
     with st.expander("**Top reasons for score**"):
-            st.write("1. Low external source mean score")
-            st.write("2. High DEBT TO INCOME RATIO")
-            st.write("3. Low bureau score")
-            st.write("4. Unstable employment")
+        st.write("1. Low external source mean score")
+        st.write("2. High DEBT TO INCOME RATIO")
+        st.write("3. Low bureau score")
+        st.write("4. Unstable employment")
 else:
     st.error("High risk of default. Consider improving credit profile.")
 
