d26e62a99e7259b7131274f2cd143fd1054654bf
Rotshila Luvhengo <luvhengork@gmail.com>
2026-07-23 06:10:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ report_data = {
 report_df = pd.DataFrame(report_data)
 
 csv = report_df.to_csv(index=False).encode('utf-8')
-st.download_button(
+    st.download_button(
         label="📥 Download Prediction Report as CSV",
         data=csv,
     file_name=f"loan_prediction_{decision}_{prob:.0%}.csv",
